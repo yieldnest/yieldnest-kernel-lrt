a31b2c9790f6e08f01573518787986b4db630a9a
Dan OneTree <dan13ram@gmail.com>
2024-12-12 17:47:25 +0530

    fixed solhint issues

--- test/unit/mocks/MockERC20LowDecimals.sol
+++ test/unit/mocks/MockERC20LowDecimals.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.24;
 
-import {ERC20} from "src/Common.sol";
+import {ERC20} from "lib/yieldnest-vault/src/Common.sol";
 
 contract MockERC20LowDecimals is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
