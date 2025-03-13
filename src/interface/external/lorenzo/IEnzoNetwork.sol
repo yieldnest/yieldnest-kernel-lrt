4b80a23f49eb7e26a64c4857ee6eb5449ac5f8c2
danoctavian <danoctavian91@gmail.com>
2025-03-13 18:30:03 +0200

    handle enzo testing

--- src/interface/external/lorenzo/IEnzoNetwork.sol
+++ src/interface/external/lorenzo/IEnzoNetwork.sol
@@ -9,4 +9,35 @@ interface IEnzoNetwork {
      * @param _amount Amount of tokens to deposit
      */
     function deposit(address _strategy, address _token, uint256 _amount) external;
+
+    /**
+     * @notice Adds strategies to the whitelist
+     * @param _strategies Array of strategy addresses to whitelist
+     */
+    function addStrategyWhitelisted(address[] calldata _strategies) external;
+
+    /**
+     * @notice Checks if a strategy is whitelisted
+     * @param _strategy Address of the strategy to check
+     * @return True if the strategy is whitelisted, false otherwise
+     */
+    function isWhitelisted(address _strategy) external view returns (bool);
+
+    /**
+     * @notice Returns the address of the DAO
+     * @return Address of the DAO
+     */
+    function dao() external view returns (address);
+
+    /**
+     * @notice Checks if the network is paused
+     * @return True if the network is paused, false otherwise
+     */
+    function paused() external view returns (bool);
+
+    /**
+     * @notice Unpauses the network
+     * @dev Can only be called by authorized addresses
+     */
+    function unpause() external;
 }
